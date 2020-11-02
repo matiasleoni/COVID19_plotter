c47fd59ed2b8852eef2a8745df802d0cb8d7492d
Cabra <matiasleoni@gmail.com>
2020-11-02 14:58:32 -0300

    Main Code with clearer docstrings

--- manycountries6_0.py
+++ manycountries6_0.py
@@ -22,15 +22,7 @@ COLUMNS = ['iso_code', 'continent', 'location', 'date',
            'male_smokers', 'handwashing_facilities', 'hospital_beds_per_thousand', 'life_expectancy',
            'human_development_index']
 
-#####################################################
-#####################################################
-def parse_title(a_string):
-    '''
-    Take a string such as 'new_deaths_per_million' and return 'New deaths per million'
-    '''
-    a_list = a_string.split('_')
-    b_string = (' '.join(a_list)).capitalize()
-    return b_string
+
 
 ##################################################
 ##### GENERIC FUNCTION rolling: dataset -> dataset
@@ -39,7 +31,7 @@ def parse_title(a_string):
 
 def rolling_n_deep_avg(alist,n_deep):
     '''
-    INPUT: a list
+    INPUT: a list, integer n_deep
     OUTPUT: rolling "n_deep" data average list ("n_deep"-1 elements shorter).
             Integer rounded result
     '''
@@ -64,6 +56,14 @@ def transdate(string):
     year = int(string[:4])
     #print(day, month, year)
     return datetime.date(year, month, day)
+
+def parse_title(a_string):
+    '''
+    Take a string such as 'new_deaths_per_million' and return 'New deaths per million'
+    '''
+    a_list = a_string.split('_')
+    b_string = (' '.join(a_list)).capitalize()
+    return b_string
 
 #####################################################
 #####################################################
@@ -105,8 +105,9 @@ def parser_local(countries):
 
 def order_for_plot(mydic, country, init_date, type_graph, deep):
     '''
-    INPUT: - Parsed dictionary with all the data corresponding to selected countries
-           - a single country
+    INPUT: - Parsed dictionary with all the data corresponding to selected countries. Assumed the form
+             returned by parser_local function
+           - a single country (string)
            - initial date to consider in format 'YYYY-MM-DD'
            - type_graph data we are interested in
            - integer deep level or rolling average
@@ -129,7 +130,6 @@ def order_for_plot(mydic, country, init_date, type_graph, deep):
         for iter in range(deep-1):
             new_date.pop(0)
         return pd.Series(data=new_data, index=new_date)
-        #return [[new_date[index], new_data[index]] for index in range(len(new_date))]
 
 
 #####################################################
@@ -143,8 +143,8 @@ def draw_many_lines(countries, data_type, deep=7, init_date='2019-12-31', output
            - depth of the moving average. Default being 7
            - Initial date to consider in format 'YYYY-MM-DD'. Default being the whole COVID series.
            - name of output file. If parameter omitted plot is rendered in the browser
-    OUTPUT: Produces an svg plot rendered in the browser/file for the specified countries
-            and data type with a deep-day rolling mean.
+    OUTPUT: Produces a png plot rendered in a temporary file (unless outputfile chosen)
+            for the specified countries and data type with a deep-day rolling mean.
     RETURN: None
     """
     plt.style.use('seaborn-notebook')
@@ -168,10 +168,8 @@ def draw_many_lines(countries, data_type, deep=7, init_date='2019-12-31', output
         my_web = requests.get(WEB, allow_redirects=True)
         with open('owid-covid-data.csv', 'wb') as newdatafile:
             newdatafile.write(my_web.content)        
-    #mydic = parser(countries)
     print('OK')
     mydic = parser_local(countries)
-    #mydic[('Argentina', '2020-10-02')]['new_deaths_per_million'] = '8.2'
     for pais in countries:
         print('Processing data for',pais,'... ', end='')
         if data_type in COLUMNS:
@@ -185,7 +183,6 @@ def draw_many_lines(countries, data_type, deep=7, init_date='2019-12-31', output
             return      
         if len(mytable) > 0:
             print('OK')
-            #lineplot.add(pais, mytable)
             if deep == 1:
                 plot_title = parse_title(data_type)+". dataset OWID"
             elif deep > 1:
@@ -201,12 +198,9 @@ def draw_many_lines(countries, data_type, deep=7, init_date='2019-12-31', output
             return       
     if outputfile == 0:
         print('Preparing plot... ', end='')
-        #lineplot.render_in_browser()
-        #plt.show()
         plt.savefig('temporalMLO2511.png')
         fooooo = pili.open('temporalMLO2511.png').show()
         os.remove('temporalMLO2511.png')
-        #mpld3.show()
         print('OK')
     else:
         print('Saving plot... ', end='')
@@ -214,126 +208,103 @@ def draw_many_lines(countries, data_type, deep=7, init_date='2019-12-31', output
         print('OK')
         print('File saved to',outputfile)
 
-print('======================================================================================================')
-print('')
-print('                                  PLOT COVID-19 DATA                                                  ')
-print('This program is designed to plot time series lines for different COVID-19 data of different countries.')
-print('')
-print('Credit for the dataset:')
-print('Hasell, J., Mathieu, E., Beltekian, D. et al. A cross-country database of COVID-19 testing.')
-print('Sci Data 7, 345 (2020). https://doi.org/10.1038/s41597-020-00688-8')
-print('======================================================================================================')
 
-more = True
-country_list = []
-country = input('Input country name in english (e.g. United States): ')
-country_list.append(country.title())
-while more:
+def input_output_execution():
+    '''
+    INPUT: -
+    RETURN: -
+    OUTPUT: after executing input questions to the user ir executes draw_many_lines function with the proper arguments
+    '''
+    ### GENERAL INFORMATION
+    print('======================================================================================================')
+    print('')
+    print('                                  PLOT COVID-19 DATA                                                  ')
+    print('This program is designed to plot time series lines for different COVID-19 data of different countries.')
+    print('')
+    print('Credit for the dataset:')
+    print('Hasell, J., Mathieu, E., Beltekian, D. et al. A cross-country database of COVID-19 testing.')
+    print('Sci Data 7, 345 (2020). https://doi.org/10.1038/s41597-020-00688-8')
+    print('======================================================================================================')
+    ### INPUT Country names in English
+    more = True
+    country_list = []
+    country = input('Input country name in english (e.g. United States): ')
+    country_list.append(country.title())
+    while more:
+        print()
+        print('Input another country name in english (e.g. Argentina).')
+        country = input('If no more countries are needed just press ENTER: ')
+        if country != '':
+            country_list.append(country.title())
+        else:
+            more = False
+    print('')
+    print('Your set of country choices is:     ', country_list)
+    ### Present data types
     print()
-    print('Input another country name in english (e.g. Argentina).')
-    country = input('If no more countries are needed just press ENTER: ')
-    if country != '':
-        country_list.append(country.title())
-    else:
-        more = False
-print('')
-print('Your set of country choices is:     ', country_list)
-
-print()
-print('=====================================================================')
-print()
-print()
-print('LIST OF DATA TYPES')
-print('----------------------------------------------------------------------')
-print("total_cases, new_cases, total_deaths, new_deaths, \
-total_cases_per_million")
-print("new_cases_per_million,\
-total_deaths_per_million, new_deaths_per_million")
-print("total_tests, new_tests, total_tests_per_thousand,\
- new_tests_per_thousand")
-print("tests_per_case, positive_rate, tests_units, stringency_index")
-print('----------------------------------------------------------------------')
-print()
-
-more = True
-while more:
-    print('Please choose a data type from the previous list.')
-    data_type = input('Write it literally as in the list (e.g. new_cases): ')
-    if data_type in COLUMNS:
-        more = False
-    else:
-        print('')
-        print('Check your spelling. Try again')
-        print('')
-        
-print()
-print()
-
-
-while True:
-    try:
-        print('Input an integer number (days) for smoothing data with moving average.')
-        deep_avg = input('1 is the default. 7 is the recommended for daily data: ')
-        if deep_avg == '':
-            deep_avg = 1
+    print('=====================================================================')
+    print()
+    print()
+    print('LIST OF DATA TYPES')
+    print('----------------------------------------------------------------------')
+    print("total_cases, new_cases, total_deaths, new_deaths, total_cases_per_million")
+    print("new_cases_per_million, total_deaths_per_million, new_deaths_per_million")
+    print("total_tests, new_tests, total_tests_per_thousand, new_tests_per_thousand")
+    print("tests_per_case, positive_rate, tests_units, stringency_index")
+    print('----------------------------------------------------------------------')
+    print()
+    ### INPUT Data types
+    more = True
+    while more:
+        print('Please choose a data type from the previous list.')
+        data_type = input('Write it literally as in the list (e.g. new_cases): ')
+        if data_type in COLUMNS:
+            more = False
         else:
-            deep_avg = int(deep_avg)
-        break
-    except ValueError:
-        print('')
-        print('That was not an integer number. Please try again')
-        print('')
-        
-print()
-print()
-
-# print('Input starting date in format YYYY-MM-DD.')
-# init_date = input('For example 2020-06-30. Press ENTER for default (beginning of the pandemic):')
-# if init_date == '':
-#     init_date = '2019-12-31'
-
-while True:
-    try:
-        print('Input starting date in format YYYY-MM-DD.')
-        init_date = input('For example 2020-06-30. Press ENTER for default (beginning of the pandemic):')
-        if init_date == '':
-            init_date = '2019-12-31'
-        else:
-            transdate(init_date)
-        break
-    except ValueError:
-        print('')
-        print('Non-existent date or incorrect format. Please try again')
-        print('')
-
-print()
-print()
-
-print('Input the file name for the output (e.g: grafico.png).')
-outputfile = input('Press ENTER to show the plot without saving it: ')
-if outputfile == '':
-    outputfile = 0
-
-
-draw_many_lines(country_list, data_type, deep_avg, init_date, outputfile)
-
-##print('=============================================')
-##print('You may use the following data types to plot:')
-##print('=============================================')
-##print('')
-##print('')
-##print("'total_cases', 'new_cases', 'new_cases_smoothed', 'total_deaths',\
-##'new_deaths', 'new_deaths_smoothed', 'total_cases_per_million', 'new_cases_per_million',\
-##'new_cases_smoothed_per_million', 'total_deaths_per_million', 'new_deaths_per_million',\
-##'new_deaths_smoothed_per_million', 'total_tests', 'new_tests', 'total_tests_per_thousand',\
-##'new_tests_per_thousand', 'new_tests_smoothed', 'new_tests_smoothed_per_thousand',\
-##'tests_per_case', 'positive_rate', 'tests_units', 'stringency_index'")
-##print('')
-##print('')
-##print('with the command:')
-##print('')
-##print('       '+\
-##      ">>>  draw_many_lines(country_list, data_type, deep_avg, init_date='2019-12-31', outputfile=0)")
-##print('')
-##print('Omitting the last argument renders the plot in web browser')
+            print('')
+            print('Check your spelling. Try again')
+            print('')        
+    print()
+    print()
+    ### INPUT integer number corresponding to the number of days for the moving average
+    while True:
+        try:
+            print('Input an integer number (days) for smoothing data with moving average.')
+            deep_avg = input('1 is the default. 7 is the recommended for daily data: ')
+            if deep_avg == '':
+                deep_avg = 1
+            else:
+                deep_avg = int(deep_avg)
+            break
+        except ValueError:
+            print('')
+            print('That was not an integer number. Please try again')
+            print('')            
+    print()
+    print()
+    ### INPUT the desired starting date
+    while True:
+        try:
+            print('Input starting date in format YYYY-MM-DD.')
+            init_date = input('For example 2020-06-30. Press ENTER for default (beginning of the pandemic):')
+            if init_date == '':
+                init_date = '2019-12-31'
+            else:
+                transdate(init_date)
+            break
+        except ValueError:
+            print('')
+            print('Non-existent date or incorrect format. Please try again')
+            print('')
+    print()
+    print()
+    ### INPUT the name of the file to render the plot
+    print('Input the file name for the output (e.g: grafico.png).')
+    outputfile = input('Press ENTER to show the plot without saving it: ')
+    if outputfile == '':
+        outputfile = 0
+    ### EXECUTE
+    draw_many_lines(country_list, data_type, deep_avg, init_date, outputfile)
+    
+input_output_execution()
 
